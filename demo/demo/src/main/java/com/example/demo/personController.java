2cbbaaa6d8892cb7679320653ca40a9c3fe19abb
tgh1001 <78226641+tgh1001@users.noreply.github.com>
2022-09-01 12:28:34 -0400

    Add files via upload

--- demo/demo/src/main/java/com/example/demo/personController.java
+++ demo/demo/src/main/java/com/example/demo/personController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +9,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
+
+
+import com.example.demo.mysql.Repo;
+import com.example.demo.mysql.User;
+
+
 
 @Controller
-@RestController
 public class personController {
-	@Autowired
-	@RequestMapping({"/doods"})
-	public String getController() {
-		return "people";
+	private Repo repo;
+	
+	public personController(Repo repo) {
+		this.repo=repo;
 	}
+	
 	@RequestMapping({"/"})
-	public String testController() {
-		return "men";
+	public String getMapping(Model model) {
+		model.addAttribute("users", repo.findAll());
+		return "index";
+	}
+	@RequestMapping({"/tests"})
+	public String html(User user) {
+		return "add-user";
+	}
+	@RequestMapping({"/adduser"})
+	public String addUser(@Validated User user, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "add-user";
+		}
+		repo.save(user);
+		return "redirect:/index";
 	}
 }
+
